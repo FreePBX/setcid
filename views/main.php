abe5f4e92c06132d41635983c848b02b5f128087
Michael Newton <miken32@gmail.com>
2017-04-28 15:19:46 -0700

    display usage info

--- views/main.php
+++ views/main.php
@@ -2,6 +2,17 @@
 	<div class="row">
 		<div class="col-sm-12">
 			<h1><?php echo _("Set CID")?></h1>
+<?php if (!empty($usage_list)):?>
+			<div class="panel panel-default fpbx-usageinfo">
+				<div class="panel-heading">
+					<?php echo $usage_list['text']?>
+				</div>
+				<div class="panel-body">
+					<?php echo $usage_list['tooltip']?>
+				</div>
+			</div>
+
+<?php endif?>
 			<div class="fpbx-container">
 				<?php echo $content?>
 			</div>
