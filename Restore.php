b93521c25fed595727b0851b22c97571afe0c516
kapil gupta <kgupta@sangoma.com>
2019-06-03 11:54:38 +0530

    Fixing FREEPBX-20046 Backup whoops error

--- Restore.php
+++ Restore.php
@@ -3,7 +3,7 @@ namespace FreePBX\modules\Setcid;
 use FreePBX\modules\Backup as Base;
 class Restore Extends Base\RestoreBase{
 	public function runRestore($jobid){
-		$configs = $this->getConfig();
+		$configs = $this->getConfigs();
 		foreach($configs as $config){
 				$this->FreePBX->Setcid->update($config['cid_id'], $config['description'], $config['name'], $config['number'], $config['dest']);
 		}
