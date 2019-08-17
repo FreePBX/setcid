b699a4f79d18cc9b1787f98edec18a92385920f4
nthumann <me@n-thumann.de>
2019-08-17 22:14:42 +0000

    Related to FREEPBX-20414: removed $jobid from runRestore()

--- Restore.php
+++ Restore.php
@@ -2,7 +2,7 @@
 namespace FreePBX\modules\Setcid;
 use FreePBX\modules\Backup as Base;
 class Restore Extends Base\RestoreBase{
-	public function runRestore($jobid){
+	public function runRestore(){
 		$configs = $this->getConfigs();
 		foreach($configs as $config){
 				$this->FreePBX->Setcid->update($config['cid_id'], $config['description'], $config['name'], $config['number'], $config['dest']);
