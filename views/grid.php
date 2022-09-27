7dade68c23391f84740b05588c370b9eb62975fb
Megha BM <meghabm@sangoma.com>
2022-09-27 05:41:12 +0000

    New Bootstrap GUI fixes

--- views/grid.php
+++ views/grid.php
@@ -1,7 +1,7 @@
 <div class="panel panel-info">
 	<div class="panel-heading">
 		<div class="panel-title">
-			<a href="#" data-toggle="collapse" data-target="#moreinfo"><i class="glyphicon glyphicon-info-sign"></i></a>&nbsp;&nbsp;&nbsp;<?php echo _('What is Set CID?')?></div>
+			<a href="#" data-toggle="collapse" data-target="#moreinfo"><i class="fa fa-info-circle"></i></a>&nbsp;&nbsp;&nbsp;<?php echo _('What is Set CID?')?></div>
 	</div>
 	<!--At some point we can probably kill this... Maybe make is a 1 time panel that may be dismissed-->
 	<div class="panel-body collapse" id="moreinfo">
@@ -15,7 +15,7 @@
 	<thead>
 		<tr>
 			<th data-field="description"><?php echo _("Description")?></th>
-			<th data-field="actions" class="col-xs-2"><?php echo _("Actions")?></th>
+			<th data-field="actions" class="col-sm-2"><?php echo _("Actions")?></th>
 		</tr>
 	</thead>
 </table>
